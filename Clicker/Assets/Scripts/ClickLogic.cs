efcb07c398e185af50c6d518722b3ecc7d992765
Viwka <vwko111@gmail.com>
2023-02-01 19:12:05 +0300

    Pos specific click
    Added click logic to button with image

---
+++ Clicker/Assets/Scripts/ClickLogic.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class ClickLogic : MonoBehaviour
+{
+    [SerializeField] private int _hearts;
+    public void OnClick(){
+        _hearts++;
+        Debug.Log("123");
+    }
+}
