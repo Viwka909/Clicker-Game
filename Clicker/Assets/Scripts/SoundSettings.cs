5de403d25a4168696cc49038752f89ec6674ab49
Podrochimaru <kostya.arn@gmail.com>
2023-05-29 17:03:26 +0300

    Updated sound settings again
    Made other buttons inactive while the settings is open

--- Clicker/Assets/Scripts/SoundSettings.cs
+++ Clicker/Assets/Scripts/SoundSettings.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,26 +8,35 @@ using UnityEngine.UI;
 public class SoundSettings : MonoBehaviour
 {
     [SerializeField] private GameObject _settingsButton;
-    [SerializeField] private Button _button;
     private bool _isEnabled = false;
+    Button[] _buttons;
+
     private void Start()
     {
-        _button.onClick.AddListener(SoundSettingsPanel);
+        _buttons = GameObject.FindGameObjectsWithTag("ScreenButton").Select(obj => obj.GetComponent<Button>()).ToArray();
     }
     private void Update()
     {
         PanelClose();
     }
-    private void SoundSettingsPanel()
+    public void SoundSettingsPanel()
     {
         if (_isEnabled == false)
         {
             _settingsButton.SetActive(true);
+            foreach (Button button in _buttons)
+            {
+                button.interactable = false;
+            }
             _isEnabled = true;
         }
         else if (_isEnabled == true) 
         {
             _settingsButton.SetActive(false);
+            foreach (Button button in _buttons)
+            {
+                button.interactable = true;
+            }
             _isEnabled = false;
         }
     }
@@ -35,6 +45,10 @@ public class SoundSettings : MonoBehaviour
         if (_isEnabled == true && Input.GetKeyDown(KeyCode.Escape) )
         {
             _settingsButton.SetActive(false);
+            foreach (Button button in _buttons)
+            {
+                button.interactable = true;
+            }
             _isEnabled = false;
         }
     }
