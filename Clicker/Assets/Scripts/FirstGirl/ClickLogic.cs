e175023506fd7a851c62214fb2de031d10790027
Viwka <vwko111@gmail.com>
2023-06-01 00:28:03 +0300

    Most of the UI

--- Clicker/Assets/Scripts/FirstGirl/ClickLogic.cs
+++ Clicker/Assets/Scripts/FirstGirl/ClickLogic.cs
@@ -8,13 +8,18 @@ public class ClickLogic : MonoBehaviour
     [SerializeField] public int _hearts = 0;
 
     public int clickcounter = 0;
+    public Animator Stroke;
+    
     public GirlData hrtval;
     public int modifier = 1;
     public Text HeartsValue;
+    public GameObject StrokeObject;
     
 
     public void OnClick()
     {
+        StrokeObject.SetActive(true);
+        Stroke.SetTrigger("Click");
         Debug.Log(modifier);
         _hearts += 1 * modifier;
         hrtval.Value += 1 * modifier;
