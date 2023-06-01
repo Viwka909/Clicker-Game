84d2188dfb63da48e0f98496537d6c0ee125f93c
Viwka <vwko111@gmail.com>
2023-06-01 13:26:36 +0300

    Cleared Unsued Assets

---
+++ Clicker/Assets/Scripts/SelectScreen/UnlockChecker.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockChecker : MonoBehaviour
+{
+    [SerializeField]
+    public GirlUnlockCheck Girl1;
+    public GirlUnlockCheck Girl2;
+    public GirlUnlockCheck Girl3;
+    public GirlUnlockCheck Girl4;
+    void Start()
+    {
+        
+    }
+}
